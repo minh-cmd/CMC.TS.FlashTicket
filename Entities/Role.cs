8bf8f517894e95c9c354a4c27c4fc0992e7c20ed
bui hieu minh <thanhniende@gmail.com>
2026-08-24 16:31:09 +0700

    feat: adjust Role, User entities

--- Entities/Role.cs
+++ Entities/Role.cs
@@ -2,13 +2,12 @@
 {
     public class Role
     {
-        public Guid Id { get; set; }
+        public Guid RoleId { get; set; }
         public string RoleName { get; set; }
         public DateTime CreateAt {  get; set; }
         public DateTime? UpdateAt {  get; set; }
         public Guid CreateBy {  get; set; }
         public Guid UpdateBy {  get; set; }
-
         public bool IsDeleted { get; set; }
         
         public Role()
--- Entities/User.cs
+++ Entities/User.cs
@@ -2,12 +2,15 @@
 {
     public class User
     {
-        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
         public string Name { get; set; }
         public string Email {  get; set; }
-        public Guid RoleId {  get; set; }
-        public Guid CreatedAt {  get; set; }
-        public bool IsDeleted {  get; set; }
+        public string PasswordHash { get; set; }
+        public DateTime CreateAt { get; set; }
+        public DateTime? UpdateAt { get; set; }
+        public Guid CreateBy { get; set; }
+        public Guid UpdateBy { get; set; }
+        public bool IsDeleted { get; set; }
         public User() { }
 
         public User(Guid id, string name, string email, Guid roleId)
