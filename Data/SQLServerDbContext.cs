1c4d22cbd444045c085037a4d873d655a615449a
bui hieu minh <thanhniende@gmail.com>
2026-08-24 16:00:42 +0700

    feat: adding and adjust RBAC of the project(user, role, rolepermission, userrole)

--- Data/SQLServerDbContext.cs
+++ Data/SQLServerDbContext.cs
@@ -5,8 +5,12 @@ namespace CMC.TS.FT.Api.Data
 {
     public class SQLServerDbContext : DbContext
     {
-        DbSet<User> users;
-        DbSet<Role> roles;
+        DbSet<User> User;
+        DbSet<Role> Role;
+        DbSet<RolePermission> RolePermission;
+        DbSet<UserRole> UserRole;
+
+
         public SQLServerDbContext(DbContextOptions builder) : base(builder)
         {
         }
@@ -19,14 +23,46 @@ namespace CMC.TS.FT.Api.Data
 
             modelBuilder.Entity<User>(entity =>
             {
-                entity.HasKey(x => x.Id);
+                entity.HasKey(x => x.UserId);
 
-                entity.HasOne
+                entity.Property(d=>d.Email).IsRequired();
+
+                entity.HasIndex(d=>d.Email).IsUnique();
+
+                entity.Property(d => d.PasswordHash).IsRequired();
             });
 
             modelBuilder.Entity<Role>(entity =>
             {
-                entity.HasKey(x => x.Id);
+                entity.HasKey(x => x.RoleId);
+
+                entity.HasIndex(x => x.RoleName).IsUnique();
+
+                entity.Property(x => x.RoleName).IsRequired();
+            });
+
+            modelBuilder.Entity<Permission>(entity =>
+            {
+                entity.HasKey(x => x.PermissionId);
+
+                entity.HasIndex(x => x.PermissionName).IsUnique();
+
+                entity.Property(x => x.PermissionName).IsRequired();
+            });
+
+            modelBuilder.Entity<UserRole>(entity =>
+            {
+                entity.HasKey(dt => new { dt.UserId, dt.RoleId});
+                entity.HasOne<User>().WithMany().HasForeignKey(u=> u.UserId);
+                entity.HasOne<Role>().WithMany().HasForeignKey(r => r.RoleId);
+
+            });
+
+            modelBuilder.Entity<RolePermission>(entity =>
+            {
+                entity.HasKey(dt => new { dt.PermissionId, dt.RoleId });
+                entity.HasOne<Role>().WithMany().HasForeignKey(r=>r.RoleId);
+                entity.HasOne<Permission>().WithMany().HasForeignKey(p=>p.PermissionId);
             });
         }
     }
