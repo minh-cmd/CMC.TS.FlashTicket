f2f725f7af825bcda2f3b7f0a8c32bc7435762cf
bui hieu minh <thanhniende@gmail.com>
2026-08-25 15:13:10 +0700

    fix: get rid of delete log field in audit abstract class. Update database

--- Entities/AuditAbstractEntity.cs
+++ Entities/AuditAbstractEntity.cs
@@ -6,7 +6,5 @@
         public DateTime? UpdateAt { get; set; }
         public Guid CreateBy { get; set; }
         public Guid UpdateBy { get; set; }
-        public bool IsDeleted { get; set; }
-        public DateTime? DeletedAt { get; set; }
     }
 }
---
+++ Migrations/20260825081125_DeleteLogField.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CMC.TS.FT.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteLogField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "UserRole");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "UserRole");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "RolePermission");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "RolePermission");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Role");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Role");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Permission");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Permission");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "UserRole",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "UserRole",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "User",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "User",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "RolePermission",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "RolePermission",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Role",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Role",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Permission",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Permission",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
