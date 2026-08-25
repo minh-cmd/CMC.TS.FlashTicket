3892c323a53e66fb7feb725e719bf1f48cfcaad4
bui hieu minh <thanhniende@gmail.com>
2026-08-25 11:01:21 +0700

    feat: add-migration after refactor to make every class inherit abstract audit class

--- Migrations/20260825035957_EntitiesInheritAbstractAuditClass.Designer.cs
+++ Migrations/20260825035957_EntitiesInheritAbstractAuditClass.Designer.cs
@@ -4,6 +4,7 @@ using CMC.TS.FT.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CMC.TS.FT.Api.Migrations
 {
     [DbContext(typeof(SQLServerDbContext))]
-    partial class SQLServerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260825035957_EntitiesInheritAbstractAuditClass")]
+    partial class EntitiesInheritAbstractAuditClass
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20260825035957_EntitiesInheritAbstractAuditClass.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CMC.TS.FT.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class EntitiesInheritAbstractAuditClass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "UserRole",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "User",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "RolePermission",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Role",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Permission",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "UserRole");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "RolePermission");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Role");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Permission");
+        }
+    }
+}
