4160c91f1cf78d0705077653cd151e69211e3a15
bui hieu minh <thanhniende@gmail.com>
2026-08-27 10:45:10 +0700

    feat: adding permisson display endpoint and service

---
+++ Controllers/PermissionController.cs
@@ -0,0 +1,29 @@
+﻿using CMC.TS.FT.Api.Entities;
+using CMC.TS.FT.Api.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMC.TS.FT.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PermissionController : ControllerBase
+    {
+        private readonly PermissionService _permissionService;
+
+        public PermissionController(PermissionService permissionService)
+        {
+            _permissionService = permissionService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> DisplayPermission()
+        {
+            List<Permission>? a = await _permissionService.DisplayAllPermission();
+            if (a != null)
+            {
+                return Ok(a);
+            }
+            return BadRequest();
+        }
+    }
+}
---
+++ Services/PermissionService.cs
@@ -0,0 +1,32 @@
+﻿using CMC.TS.FT.Api.Entities;
+using CMC.TS.FT.Api.Repositories.GenericRepository;
+
+namespace CMC.TS.FT.Api.Services
+{
+    public class PermissionService
+    {
+        private readonly IRepository<Permission> _repository;
+        private readonly ILogger<PermissionService> _logger;
+
+        public PermissionService(IRepository<Permission> repository, ILogger<PermissionService> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        public async Task<List<Permission>?> DisplayAllPermission()
+        {
+            try
+            {
+                _logger.LogInformation("start display permission operation");
+                List<Permission>? a = await _repository.GetAll();
+                return a;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "displaying permission failed");
+                return null;
+            }
+        }
+    }
+}
