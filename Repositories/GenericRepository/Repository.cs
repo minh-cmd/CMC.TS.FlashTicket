3a31ea3b9fa988002d696f3eaa50ffaa5c140ba0
bui hieu minh <thanhniende@gmail.com>
2026-08-27 15:15:37 +0700

    refactor: delete Ilogger use in generic repository

--- Repositories/GenericRepository/Repository.cs
+++ Repositories/GenericRepository/Repository.cs
@@ -6,53 +6,30 @@ namespace CMC.TS.FT.Api.Repositories.GenericRepository
     public class Repository<T> : IRepository<T> where T : class
     {
         SQLServerDbContext _context;
-        ILogger<Repository<T>> _logger;
-
-        public Repository(SQLServerDbContext context, ILogger<Repository<T>> logger)
+        public Repository(SQLServerDbContext context)
         {
             _context = context;
-            _logger = logger;
         }
 
         public async Task<bool> Create(T? entity)
         {
-            try
+            if (entity == null)
             {
-                _logger.LogInformation("create operation start");
-                if (entity == null)
-                {
-                    _logger.LogError("entity of create operation can't be null");
-                    return false;
-                }
-                _context.Set<T>().Add(entity);
-                return await _context.SaveChangesAsync() > 0;
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, "create operation failed");
                 return false;
             }
+            _context.Set<T>().Add(entity);
+            return await _context.SaveChangesAsync() > 0;
         }
 
         public async Task<bool> Delete(Guid id)
         {
-            try
+            T? entity = await GetById(id);
+            if (entity == null) 
             {
-                _logger.LogInformation("find by id operation start");
-                T? entity = await GetById(id);
-                if (entity == null) 
-                {
-                    _logger.LogError("there is no object {id}", id);
-                    return false;
-                }
-                _context.Set<T>().Remove(entity);
-                return await _context.SaveChangesAsync() > 0;
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, "find by id operation failed");
                 return false;
             }
+            _context.Set<T>().Remove(entity);
+            return await _context.SaveChangesAsync() > 0;
         }
 
         public async Task<List<T>?> GetAll()
@@ -62,43 +39,23 @@ namespace CMC.TS.FT.Api.Repositories.GenericRepository
 
         public async Task<T?> GetById(Guid id)
         {
-            try
-            {
-                _logger.LogInformation("find by id operation start");
-                return await _context.Set<T>().FindAsync(id);
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, "find by id operation failed");
-                return null;
-            }
+            return await _context.Set<T>().FindAsync(id);
         }
 
         public async Task<bool> Update(T? entity)
         {
-            try
+            if (entity == null)
             {
-                _logger.LogInformation("update operation start");
-                if (entity == null)
-                {
-                    _logger.LogError("there is no object");
-                    return false;
-                }
-                _context.Set<T>().Update(entity);   
-                bool isSuccess = await _context.SaveChangesAsync() > 0;
-                if (isSuccess)
-                {
-                    return true;
-                }
-                else
-                {
-                    _logger.LogError("update operation affect 0 row");
-                    return false;
-                }
+                return false;
             }
-            catch (Exception e)
+            _context.Set<T>().Update(entity);   
+            bool isSuccess = await _context.SaveChangesAsync() > 0;
+            if (isSuccess)
             {
-                _logger.LogError(e, "find by id operation failed");
+                return true;
+            }
+            else
+            {
                 return false;
             }
         }
