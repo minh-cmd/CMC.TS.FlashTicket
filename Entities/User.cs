5ee3caa2304b0bd77c7befc3c545808574d83f4c
bui hieu minh <thanhniende@gmail.com>
2026-09-03 15:01:52 +0700

    feat: adding IsDeleted and IsActive field for sqlserver

--- Entities/User.cs
+++ Entities/User.cs
@@ -6,6 +6,8 @@
         public string Name { get; set; }
         public string Email {  get; set; }
         public string PasswordHash { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
         public User() { }
        
     }
