0eac5424049cfb50b5fb7d789caf41ffb7bfb0bd
bui hieu minh <thanhniende@gmail.com>
2026-09-21 15:05:05 +0700

    feat: changing to use postgresql

--- Program.cs
+++ Program.cs
@@ -54,7 +54,7 @@ namespace CMC.TS.FT.Api
                     }
                 });
             });
-            builder.Services.AddDbContext<SQLServerDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("SQLServerConnection")));
+            builder.Services.AddDbContext<SQLServerDbContext>(option => option.UseNpgsql(builder.Configuration.GetConnectionString("PostgresqlConnection")));
             builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
             builder.Services.AddScoped<PermissionService>();
             builder.Services.AddScoped<IRoleRepository, RoleRepository>();
